7c45893d7a88efb1abb26ba48d52af386e05397c
Claire Chabas <claire.chabas@gmail.com>
2023-01-20 11:51:37 +0000

    chore: simple implementtion of the observer pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import observable from "./Observable";
+import { Button, FormControlLabel, Switch } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+function handleClick() {
+  observable.notify("User clicked the button!");
+}
+function handleToggle() {
+  observable.notify("User toggled the switch!");
+}
+function logger(data) {
+  console.log(`${Date.now()} ${data}`);
+}
+function toastify(data) {
+  toast(data, {
+    position: toast.POSITION.BOTTOM_RIGHT,
+    closeButton: false,
+    autoClose: 2000,
+  });
+}
+
+observable.subscribe(logger);
+observable.subscribe(toastify);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        poition: "relative",
+        top: "50%",
+      }}
+    >
+      <Button variant="contained" onClick={handleClick}>
+        Click me!
+      </Button>
+      <FormControlLabel
+        style={{ marginTop: "2rem" }}
+        control={<Switch name="" onChange={handleToggle} />}
+        label="Toggle me!"
+      />
+      <ToastContainer />
     </div>
   );
 }
-
-export default App;
